d5cb34be51cdfc6d291cb620cf4f4a71774b9656
AnastasiaRekalo <global-rekalonastya@gmail.com>
2023-09-10 11:22:30 +0300

    Решение 2  задачи

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int Promt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+int GetThirdRanx(int number)
+{
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    return number % 10;
+}
+bool ValidateNumber(int number)
+{
+    if (number < 100)
+    {
+        Console.WriteLine("Третьей цифры нет");
+        return false;
+    }
+    return true;
+}
+int number = Promt("Введите число >");
+if (ValidateNumber(number))
+{
+    Console.WriteLine(GetThirdRanx(number));
+}
