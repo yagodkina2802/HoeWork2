12c73bef2c8f5a52e7b96e73a7548309e4ab4d3d
AnastasiaRekalo <global-rekalonastya@gmail.com>
2023-09-10 11:26:28 +0300

    решение 3 задачи

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+Console.WriteLine("Введите номер дня недели: ");
+string num = Console.ReadLine()!;
+switch (num)
+{
+    case "1":
+    Console.WriteLine("Нет");
+    break;
+    case "2":
+    Console.WriteLine("Нет");
+    break;
+    case "3":
+    Console.WriteLine("Нет");
+    break;
+    case "4":
+    Console.WriteLine("Нет");
+    break;
+    case "5":
+    Console.WriteLine("Нет");
+    break;
+    case "6":
+    Console.WriteLine("Да");
+    break;
+    case "7":
+    Console.WriteLine("Да");
+    break;
+    default:
+    Console.WriteLine("Нет такого дня недели");
+    break;
+}
